6d13aed39718f19b41e63a30ba40621516ebcb30
faruk10 <faruk@webgentechnologies.com>
2025-05-05 13:32:58 +0000

    Backup: backup_1.sql - 2025-05-05 13:32:58

--- db_backup/backup_1.sql
+++ db_backup/backup_1.sql
@@ -346,6 +346,7 @@ COPY public.auth_permission (id, name, content_type_id, codename) FROM stdin;
 COPY public.auth_user (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) FROM stdin;
 1	pbkdf2_sha256$1000000$7UnWAjDbc6zzADQCU5RCYI$aVDwSAp+DI5oKFRtPABF/XW8R1UFbJnKa0E+Qap5YXU=	\N	t	new			new@new.com	t	t	2025-05-05 18:19:30.872259+05:30
 2	pbkdf2_sha256$1000000$yvCBqRnbT5kDHXCaSirGCD$pixFDssTDamyQz5sZM7XQ5+HfVye0VBfCIz2WdLJL2I=	\N	t	ss			ss@aa.com	t	t	2025-05-05 18:32:19.318604+05:30
+3	pbkdf2_sha256$1000000$IfNRsbtmLMwljEKasmN7SR$Cm+ZWjAm2rDOyaWYbLDSdVUycFh27k5gWKl0nNb3SpU=	\N	t	newuser			new@user.com	t	t	2025-05-05 19:02:42.65533+05:30
 \.
 
 
@@ -453,7 +454,7 @@ SELECT pg_catalog.setval('public.auth_user_groups_id_seq', 1, false);
 -- Name: auth_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
 --
 
-SELECT pg_catalog.setval('public.auth_user_id_seq', 2, true);
+SELECT pg_catalog.setval('public.auth_user_id_seq', 3, true);
 
 
 --
